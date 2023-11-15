8efd5448fe0f3c3ca4b8519ad1179feb769c4b82
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-11-15 23:27:43 +0300

    basic dialog added

--- app/src/main/kotlin/com/bookish/app/ui/elements/Dialogs.kt
+++ app/src/main/kotlin/com/bookish/app/ui/elements/Dialogs.kt
@@ -1,2 +1,53 @@
 package com.bookish.app.ui.elements
 
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.TextButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BasicDialog () {
+    val openDialog = remember { mutableStateOf( true)}
+
+    if(openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                openDialog.value = false
+            },
+            title = {
+                Text(text = "Title")
+            },
+            text = {
+                Text(text = "Turned on by default")
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openDialog.value = false
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openDialog.value = false
+                    }
+                ) {
+                    Text("Dismiss")
+                }
+            }
+        )
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewBasicDialog() {
+    BasicDialog()
+}
